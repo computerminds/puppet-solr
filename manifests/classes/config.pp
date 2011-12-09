4e20e4813a71ded5db3d50d7dc3fb5ea2327fdbd
Steven Merrill <steven.merrill@gmail.com>
2011-12-08 23:41:23 -0500

    Fix issue w/ SOLR config and NFS mount order.

--- manifests/classes/config.pp
+++ manifests/classes/config.pp
@@ -7,9 +7,23 @@ class solr::config {
 
   augeas { "solr config":
     changes => [
-      "set /files/etc/default/tomcat6/JAVA_OPTS '\"-Djava.awt.headless=true -Dsolr.solr.home=/backpacktv/config/solr -Dsolr.data.dir=/opt/solr -Xmx128m -XX:+UseConcMarkSweepGC\"'",
+      "set /files/etc/default/tomcat6/JAVA_OPTS '\"-Djava.awt.headless=true -Dsolr.solr.home=/opt/solr/config -Dsolr.data.dir=/opt/solr/data -Xmx128m -XX:+UseConcMarkSweepGC\"'",
     ],
-    require => File["/opt/solr"],
+    require => [ File["/opt/solr/config"], File["/opt/solr/data"] ],
+    notify => Service["tomcat6"],
+  }
+
+  file { "/opt/solr/data":
+    ensure => directory,
+    owner => "tomcat6",
+    group => "tomcat6",
+  }
+
+  # This is so hardcoded to backpack that it's not even funny.
+  file { "/opt/solr/config":
+    ensure => directory,
+    source => "/backpacktv/config/solr",
+    recurse => true,
     notify => Service["tomcat6"],
   }
 
